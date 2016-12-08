e490740870e63783a9866c507f16d9e7dcf89007
carmenales <carmen.aleslopez@gmail.com>
2016-12-08 19:02:44 +0100

    añadiendo codigo servidorUDP

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,28 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QTimer *tiempo = new QTimer(this);
+    connect(this, SIGNAL(clicked()), this, SLOT(close()));
+    connect(tiempo, SIGNAL(timeout()), this, SLOT(sendDatagram()));
+    tiempo->start(2*1000);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow(){
     delete ui;
+}
+
+void MainWindow::on_sendButton_clicked(){
+//    udpSocket.writeDatagram(buffer.data(),QHostAddress("192.168.1.133", 7000);
+    sendDatagram();
+}
+
+void MainWindow::sendDatagram(){
+    QByteArray datagram;
+    QDataStream out(&datagram, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_5_7);
+    out << ui->mensaje->toPlainText();
+   // udpSocket.writeDatagram(datagram, QHostAddress::LocalHost, 5824);
+    udpSocket.writeDatagram(datagram, QHostAddress("192.168.1.126"), 5824);
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QUdpSocket>
+#include <QTimer>
+
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +18,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_sendButton_clicked();
+    void sendDatagram();
+
 private:
     Ui::MainWindow *ui;
+    QUdpSocket udpSocket;
 };
 
 #endif // MAINWINDOW_H
